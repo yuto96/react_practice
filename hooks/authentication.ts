19ee6500cd7f6eac4b358f59e6076aa122696d40
yuto96 <yutom.f96@gmail.com>
2021-03-24 14:33:04 +0900

    setting firebase

---
+++ hooks/authentication.ts
@@ -0,0 +1,58 @@
+import firebase from 'firebase/app'
+import { useEffect } from 'react'
+import { atom, useRecoilState } from 'recoil'
+import { User } from '../models/User'
+
+const userState = atom<User>({
+  key: 'user',
+  default: null,
+})
+
+async function createUserIfNotFound(user: User) {
+  const userRef = firebase.firestore().collection('users').doc(user.uid)
+  const doc = await userRef.get()
+  if (doc.exists) {
+    // 書き込みの方が高いので！
+    return
+  }
+
+  await userRef.set({
+    name: 'taro' + new Date().getTime(),
+  })
+}
+
+export function useAuthentication() {
+  const [user, setUser] = useRecoilState(userState)
+
+  useEffect(() => {
+    if (user !== null) {
+      return
+    }
+
+    console.log('Start useEffect')
+
+    firebase
+      .auth()
+      .signInAnonymously()
+      .catch(function (error) {
+        // Handle Errors here.
+        console.error(error)
+      })
+
+    firebase.auth().onAuthStateChanged(function (firebaseUser) {
+      if (firebaseUser) {
+        const loginUser: User = {
+          uid: firebaseUser.uid,
+          isAnonymous: firebaseUser.isAnonymous,
+        }
+        setUser(loginUser)
+        createUserIfNotFound(loginUser)
+      } else {
+        // User is signed out.
+        setUser(null)
+      }
+    })
+  }, [])
+
+  return { user }
+}
