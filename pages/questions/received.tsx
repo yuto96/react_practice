6f8991582d28f027280afd701b62596fe2577bb9
yuto96 <yutom.f96@gmail.com>
2021-03-26 16:18:49 +0900

    create questions page

---
+++ pages/questions/received.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react'
+import firebase from 'firebase/app'
+import useAuthentication from '../../hooks/authentication'
+import { Question } from '../../models/Question'
+import Layout from '../../components/Layout'
+import dayjs from 'dayjs'
+
+const { user } = useAuthentication()
+
+useEffect(() => {
+  if (!process.browser) {
+    return
+  }
+  if (user === null) {
+    return
+  }
+
+  async function loadQuestions() {
+    const snapshot = await firebase
+    .firestore()
+    .collection('questions')
+    .where('receiverUid', '==', user.uid)
+    .orderBy('createdAt', 'desc')
+    .get()
+
+    if (snapshot.empty) {
+      return
+    }
+
+    const gotQuestions = snapshot.docs.map((doc) => {
+      const question = doc.data() as Question
+      question.id = doc.id
+      return question
+    })
+    setQuestions(gotQuestions)
+  }
+
+  loadQuestions()
+}, [process.browser, user])
+
+
+export default function QuestionsReceived() {
+  const [questions, setQuestions] = useState<Question[]>([])
+
+  return (
+    <Layout>
+      <div>{questions.length}</div>
+      <h1 className="h4">受け取った質問一覧</h1>
+
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-6">
+          {questions.map((question) => (
+            <div className="card my-3" key={question.id}>
+              <div className="card-body">
+                <div className="text-truncate">{question.body}</div>
+                <div className="text-muted text-end">
+  <small>{dayjs(question.createdAt.toDate()).format('YYYY/MM/DD HH:mm')}</small>
+</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+    </Layout>
+  )
+}
--- pages/users/[uid].tsx
+++ pages/users/[uid].tsx
@@ -1,21 +1,27 @@
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import { User } from '../../models/User'
+import Layout from '../../components/Layout'
 import firebase from 'firebase/app'
+import { toast } from 'react-toastify';
+
 
 type Query = {
     uid: string
   } 
+
 export default function UserShow() {
   const [user, setUser] = useState<User>(null)
   const router = useRouter()
   const query = router.query as Query
+  const [body, setBody] = useState('')
+  const [isSending, setIsSending] = useState(false)
+  
 
   useEffect(() => {
     if (query.uid === undefined) {
         return
       }
-    
     async function loadUser() {
       console.log(query)
       const doc = await firebase
@@ -35,5 +41,73 @@ export default function UserShow() {
     }
     loadUser()
   }, [query.uid])
-  return <div>{user ? user.name : 'ロード中…'}</div>
+  return (
+    <Layout>
+    {user && (
+      <div className="text-center">
+        <h1 className="h4">{user.name}さんのページ</h1>
+        <div className="m-5">{user.name}さんに質問しよう！</div>
+      </div>
+    )}
+    <div className="row justify-content-center mb-3">
+  <div className="col-12 col-md-6">
+  <form onSubmit={onSubmit}>
+    <textarea
+  className="form-control"
+  placeholder="おげんきですか？"
+  rows={6}
+  value={body}
+  onChange={(e) => setBody(e.target.value)}
+  required
+></textarea>
+<div className="m-3">
+  {isSending ? (
+    <div className="spinner-border text-secondary" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+  ) : (
+    <button type="submit" className="btn btn-primary">
+      質問を送信する
+    </button>
+  )}
+</div>
+    </form>
+  </div>
+</div>
+  </Layout>
+    )
+    async function onSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+      
+        await firebase.firestore().collection('questions').add({
+          senderUid: firebase.auth().currentUser.uid,
+          receiverUid: user.uid,
+          body,
+          isReplied: false,
+          createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        })
+      
+        setBody('')
+        toast.success('質問を送信しました。', {
+            position: 'bottom-left',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          })
+        setIsSending(true)
+
+await firebase.firestore().collection('questions').add({
+  senderUid: firebase.auth().currentUser.uid,
+  receiverUid: user.uid,
+  body,
+  isReplied: false,
+  createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+})
+
+setIsSending(false)
+      }
+      
 }
